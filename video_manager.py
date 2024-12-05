512100ee49bf24be94a1bee8eb94087cacfbb9b1
LepharamRamchiary <lepharamchiary@gmail.com>
2024-12-05 14:48:35 +0530

    add add, list, delet and update method

--- video_manager.py
+++ video_manager.py
@@ -7,17 +7,43 @@ def load_data():
     except FileNotFoundError:
         return []
 
+def save_data_helper(videos):
+    with open('data.txt', 'w') as file:
+        json.dump(videos, file)
+        
 def list_all_videos(videos):
-    pass
+    print("\n")
+    print("*" * 70)
+    for index, video in enumerate(videos, start=1):
+        print(f"{index}. {video['name']}, Duration: {video['time']}")  
+    print("\n")
+    print("*" * 70)
 
 def add_video(videos):
-    pass
+    name = input("Enter video name: ")
+    time = input("Enter video time: ")
+    videos.append({'name':name, 'time':time})
+    save_data_helper(videos)
  
 def update_video(videos):
-    pass
+    list_all_videos(videos)
+    index = int(input("Enter the number of the video you want to update: "))
+    if 1 <= index <= len(videos):
+        name = input("Enter new video name: ")
+        time = input("Enter new video time: ")
+        videos[index-1] = {'name':name, 'time':time}
+        save_data_helper(videos)
+    else:
+        print("Invalid index")
 
 def delete_video(videos):
-    pass
+    list_all_videos(videos)
+    index = int(input("Enter the number of the video you want to delete: "))
+    if 1 <= index <= len(videos):
+        del videos[index-1]
+        save_data_helper(videos)
+    else:
+        print("Invalid index")
 
 
 def main():
@@ -31,6 +57,7 @@ def main():
         print("5. Exit")
         
         choice = input("Enter your choice: ")
+        # print(videos)
         
         
         match choice:
