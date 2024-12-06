476ee691d9145710a0f2de6869434ad633d661f3
LepharamRamchiary <lepharamchiary@gmail.com>
2024-12-06 11:37:51 +0530

    add sqlite3 db

--- video_manager.py
+++ video_manager.py
@@ -1,78 +1,111 @@
-import json
+# import json
 
-def load_data():
-    try:
-        with open('data.txt', 'r') as file:
-            return json.load(file)
-    except FileNotFoundError:
-        return []
+import sqlite3
+con = sqlite3.connect("data.bd")
+cursor = con.cursor()
 
-def save_data_helper(videos):
-    with open('data.txt', 'w') as file:
-        json.dump(videos, file)
+cursor.execute('''
+    create table IF NOT EXISTS videos (
+        id INTEGER PRIMARY KEY,
+        name TEXT NOT NULL,
+        time TEXT NOT NULL
+    )                      
+''')
+
+# def load_data():
+#     try:
+#         with open('data.txt', 'r') as file:
+#             return json.load(file)
+#     except FileNotFoundError:
+#         return []
+
+# def save_data_helper(videos):
+    # with open('data.txt', 'w') as file:
+    #     json.dump(videos, file)
         
-def list_all_videos(videos):
+def list_all_videos():
+    cursor.execute("Select * from videos")
+    rows = cursor.fetchall()
+    
     print("\n")
-    print("*" * 70)
-    for index, video in enumerate(videos, start=1):
-        print(f"{index}. {video['name']}, Duration: {video['time']}")  
+    print("*" * 30)
+    
+    if not rows:
+        print("No videos available")
+    else:
+        for row in rows:
+            print(row)
+    
+    # for row in cursor.fetchall():
+    #     print(row)
     print("\n")
-    print("*" * 70)
+    print("*" * 30)
 
-def add_video(videos):
-    name = input("Enter video name: ")
-    time = input("Enter video time: ")
-    videos.append({'name':name, 'time':time})
-    save_data_helper(videos)
+def add_video(name, time):
+    cursor.execute("INSERT INTO videos (name, time) VALUES (?, ?)", (name, time))
+    con.commit()    
+    print(f"Video '{name}' add successfully")
  
-def update_video(videos):
-    list_all_videos(videos)
-    index = int(input("Enter the number of the video you want to update: "))
-    if 1 <= index <= len(videos):
-        name = input("Enter new video name: ")
-        time = input("Enter new video time: ")
-        videos[index-1] = {'name':name, 'time':time}
-        save_data_helper(videos)
-    else:
-        print("Invalid index")
+def update_video(video_id, new_name, new_time):
+    cursor.execute("UPDATE videos SET name = ?, time = ? WHERE id = ?", (new_name, new_time, video_id))
+    con.commit()
+    print(f"updated successfully")
+    
 
-def delete_video(videos):
-    list_all_videos(videos)
-    index = int(input("Enter the number of the video you want to delete: "))
-    if 1 <= index <= len(videos):
-        del videos[index-1]
-        save_data_helper(videos)
-    else:
-        print("Invalid index")
+def delete_video(video_id):
+    cursor.execute("DELETE from videos where id = ?", (video_id,))
+    con.commit()
+    print(f"deleted successfully")
 
 
 def main():
-    videos = load_data()
+    # videos = load_data()
     while True:
-        print("\n Video Manager | choose an option")
+        print("\n Video Manager with DB | choose an option")
         print("1. List all videos")
         print("2. Add a video")
         print("3. Update a video")
         print("4. Delete a video")
-        print("5. Exit")
+        print("5. Exit app")
         
         choice = input("Enter your choice: ")
         # print(videos)
         
+        if choice == '1':
+            list_all_videos()
+        elif choice == '2':
+            name = input("Enter the video name: ")
+            time = input("Enter the video time: ")
+            add_video(name, time)
+        elif choice == '3':
+            video_id = input("Enter video id to update: ")
+            name = input("Enter the video name: ")
+            time = input("Enter the video time: ")
+            update_video(video_id, name, time)
+        elif choice == '4':
+            video_id = input("Enter video id to Delete: ")
+            delete_video(video_id)
+        elif choice == '5':
+            break
+        else:
+            print("Invalid choice")
+    
+    con.close()
         
-        match choice:
-            case '1':
-                list_all_videos(videos)
-            case '2':
-                add_video(videos)
-            case '3':
-                update_video(videos)
-            case '4':
-                delete_video(videos)
-            case '5':
-                break
-            case _:
-                print("Invalid choice")
+        # match choice:
+            # case '1':
+            #     list_all_videos(videos)
+            # case '2':
+            #     add_video(videos)
+            # case '3':
+            #     update_video(videos)
+            # case '4':
+            #     delete_video(videos)
+            # case '5':
+            #     break
+            # case _:
+            #     print("Invalid choice")
+        
                 
 if __name__ == "__main__":
     main()
